fe67639a9b9104e013369e3049df5def76f4af94
mike <git@mmcginty.me>
2024-11-16 22:51:08 -0500

    more parsing and printing

--- xml/parse_to_md.py
+++ xml/parse_to_md.py
@@ -119,6 +119,7 @@ def add_anchors(text,part='97'):
             # processed_lines.append('</span>')
             indent = "  "*(len(current_section)-2) if len(current_section)>1 else ""
             indent += "- " if len(current_section)>1 else ""
+            processed_lines.append(anchor_html)
             processed_lines.append(indent+ line)
         else:
             processed_lines.append(line)
